5ec3019f33c3008230801566ecd8f82394947087
James K <jkrrv@jkrrv.org>
2022-02-06 00:05:00 -0500

    Clarity in comments

--- src/python/WebPublicPerson.py
+++ src/python/WebPublicPerson.py
@@ -7,20 +7,19 @@
 ## 					the code repository.
 ## Data.Info - 		This is the object that gets returned for display.  It has the following attributes and default values:
 ##					- Exclude - bool - Default: False.  Setting this to True will prevent this person from being included
-##					  in the public website completely.  This means no user account will be created for them and they
-##					  will be excluded from any public-facing directory listing.
-##					- GoesBy - string - Default: NickName if the Person has one, otherwise FirstName.  Becomes the
-##					  First_Name field in WordPress.
-##					- LastName - string - Default: LastName.  Becomes the Last_Name field in WordPress
-##					- DisplayName - string - Default: GoesBy + " " + LastName. Becomes the Display_Name field in WordPress
+##					  in the public website completely.  This means no user account will be created for them on
+##					  WordPress and they will be excluded from any People Lists.
+##					- GoesBy - string - Default: NickName if the Person has one, otherwise FirstName.
+##					- LastName - string - Default: LastName
+##					- DisplayName - string - Default: GoesBy + " " + LastName
 
 goSecurityLevel = model.ExtraValueIntFamily(Data.Person.PeopleId, "Security Level")
 
 # GO Policy:
-# 1 - full names
+# 1 - full names (no change)
 # 2 - full first names, no last name
 # 3 - first initials only
-# 4 - Exclude
+# 4 - Exclude from everything
 
 if goSecurityLevel > 1:
 	Data.Info.LastName = ""
@@ -30,4 +29,3 @@ if goSecurityLevel > 2:
 
 if goSecurityLevel > 3:
 	Data.Info.Exclude = True
-
