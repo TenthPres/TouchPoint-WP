409aded232ffbb4245cbe3cfb27cb08138ecc543
James K <jkrrv@jkrrv.org>
2023-05-01 18:49:07 -0400

    Remove github header

--- touchpoint-wp.php
+++ touchpoint-wp.php
@@ -12,12 +12,13 @@
 /*
 Plugin Name:        TouchPoint WP
 Plugin URI:         https://github.com/tenthpres/touchpoint-wp
-GitHub Plugin URI:  https://github.com/tenthpres/touchpoint-wp
+Update URI:         https://github.com/tenthpres/touchpoint-wp
 Description:        A WordPress Plugin for integrating with TouchPoint Church Management Software.
 Version:            0.0.30
 Author:             James K
 Author URI:         https://github.com/jkrrv
 License:            AGPLv3+
+Text Domain:        TouchPoint-WP
 Requires at least:  5.5
 Tested up to:       6.2
 Requires PHP:       7.4
@@ -48,12 +49,12 @@ if (TOUCHPOINT_COMPOSER_ENABLED) {
     require_once __DIR__ . "/src/TouchPoint-WP/api.php";
     require_once __DIR__ . "/src/TouchPoint-WP/Utilities/Cleanup.php";
     require_once __DIR__ . "/src/TouchPoint-WP/Utilities/Geo.php";
-	require_once __DIR__ . "/src/TouchPoint-WP/Utilities/PersonArray.php";
-	require_once __DIR__ . "/src/TouchPoint-WP/geo.php";
+    require_once __DIR__ . "/src/TouchPoint-WP/Utilities/PersonArray.php";
+    require_once __DIR__ . "/src/TouchPoint-WP/geo.php";
 
     require_once __DIR__ . "/src/TouchPoint-WP/Person.php";
     require_once __DIR__ . "/src/TouchPoint-WP/Involvement.php";
-	require_once __DIR__ . "/src/TouchPoint-WP/Location.php";
+    require_once __DIR__ . "/src/TouchPoint-WP/Location.php";
 }
 
 /*** Load (set action hooks, etc.) ***/
