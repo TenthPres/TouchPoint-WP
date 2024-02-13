ffef10b418ac031c8034834198e673450089987b
James K. <jkrrv@jkrrv.org>
2024-02-12 20:11:26 -0500

    Resolve an issue of a deprecated cast

--- src/TouchPoint-WP/Utilities.php
+++ src/TouchPoint-WP/Utilities.php
@@ -418,7 +418,7 @@ abstract class Utilities
 	 */
 	public static function createGuid(): string
 	{
-		mt_srand(( double )microtime() * 10000);
+		mt_srand(intval(microtime(true) * 10000) % PHP_INT_MAX);
 		$char   = strtoupper(md5(uniqid(rand(), true)));
 		$hyphen = chr(45); // "-"
 
