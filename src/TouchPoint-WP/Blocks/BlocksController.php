a35a190ee5ed33ef1f85b42a323b29a7fd70a2b2
James K. <jkrrv@jkrrv.org>
2025-09-08 22:22:55 -0400

    Point to non-minified js files

--- src/TouchPoint-WP/Blocks/BlocksController.php
+++ src/TouchPoint-WP/Blocks/BlocksController.php
@@ -33,7 +33,7 @@ abstract class BlocksController
 		 *
 		 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
 		 */
-//		if (function_exists('wp_register_block_types_from_metadata_collection')) {  TODO re-enable when it doesn't cause warnings in the logs. 
+//		if (function_exists('wp_register_block_types_from_metadata_collection')) {  TODO re-enable when it doesn't cause warnings in the logs.
 //			wp_register_block_types_from_metadata_collection($blocksRoot, $blocksRoot . 'blocks-manifest.php');
 //			return;
 //		}
