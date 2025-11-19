c2a4d3610703ef651694fd0c89fd17758b098ee5
James K. <jkrrv@jkrrv.org>
2025-11-18 20:51:22 -0500

    preventing an inplicit type conversion

--- src/TouchPoint-WP/Utilities/Colors.php
+++ src/TouchPoint-WP/Utilities/Colors.php
@@ -100,7 +100,7 @@ abstract class Colors
 		$a = $s * min($l, 1 - $l) / 100;
 
 		$f = function ($n) use ($h, $l, $a) {
-			$k     = ($n + $h / 30) % 12;
+			$k     = intval($n + $h / 30) % 12;
 			$color = $l - $a * max(min($k - 3, 9 - $k, 1), -1);
 
 			return round(255 * $color);
