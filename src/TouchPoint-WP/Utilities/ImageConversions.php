78ee124db6f8db5542d4030cbd5c21cdd2424563
James K. <jkrrv@jkrrv.org>
2026-03-26 13:27:58 -0400

    inspection comment that Imagick is not required. Docs update.

--- src/TouchPoint-WP/Utilities/ImageConversions.php
+++ src/TouchPoint-WP/Utilities/ImageConversions.php
@@ -17,6 +17,7 @@ abstract class ImageConversions
 	 * @throws \ImagickException
 	 * @throws TouchPointWP_Exception
 	 * @noinspection PhpFullyQualifiedNameUsageInspection - Imagick is helpful but not required.
+	 * @noinspection PhpComposerExtensionStubsInspection - Imagick is helpful but not required.
 	 */
 	public static function svgToPng($svgContent, ?string $backgroundColor = null): string
 	{
